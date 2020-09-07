1ba8f7b0c3534d676d4ff48b630415cf03424ce4
Choi <122031553@qq.com>
2020-09-07 19:04:13 +0800

    00 establish project

---
+++ TimeDilemma/Source/TimeDilemma/TimeDilemmaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TimeDilemmaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMEDILEMMA_API ATimeDilemmaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
